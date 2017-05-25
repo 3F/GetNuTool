e62818d13c26be94fc14bef0d9d984372151a248
Denis Kuzmin (reg) <entry.reg@gmail.com>
2017-05-25 20:57:04 +0300

    Our batch files requires CRLF for correct work. Fix problem of appveyor CI https://github.com/3F/hMSBuild/issues/2

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ set msbuild=netmsb
 
 
 call %msbuild% logic.targets /p:ngconfig="packages.config" /nologo /v:m /m:4 || goto err
-call %msbuild% "gnt.sln" /l:"packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll" /v:m /m:4
+call %msbuild% "gnt.sln" /l:"packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll" /v:m /m:4 || goto err
 
 exit /B 0
 
